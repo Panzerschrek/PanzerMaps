8ff2322032d13aa2b9b0200526ca4684b4a70ab6
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-06-23 13:31:07 +0700

    ispoljzujem sglaživanije.

--- source/maps/system_window.cpp
+++ source/maps/system_window.cpp
@@ -45,6 +45,7 @@ SystemWindow::SystemWindow()
 
 	const bool fullscreen= false;
 	const bool vsync= true;
+	const int msaa_samples= 4; // 0 - ni multisampling.
 
 	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
 	SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 24 );
@@ -56,6 +57,12 @@ SystemWindow::SystemWindow()
 	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
 	SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
 	SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+
+	if( msaa_samples > 0 )
+	{
+		SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 1 );
+		SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, msaa_samples );
+	}
 
 	window_=
 		SDL_CreateWindow(
