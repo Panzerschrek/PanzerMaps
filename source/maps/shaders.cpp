3098aa6093e637be5c0aadb4ce3e567bcb7e261d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-06-29 19:06:44 +0700

    Vynos koda šejederov v otdeljnyj fajl.

---
+++ source/maps/shaders.cpp
@@ -0,0 +1,93 @@
+#include "shaders.hpp"
+
+#define GLSL_VERSION "#version 330\n"
+
+namespace PanzerMaps
+{
+
+namespace Shaders
+{
+
+const char point_vertex[]=
+GLSL_VERSION
+R"(
+	uniform mat4 view_matrix;
+	in vec2 pos;
+	in float color_index;
+	out vec4 f_color;
+	void main()
+	{
+		f_color= vec4( mod( color_index, 2.0 ), mod( color_index, 4.0 ) / 3.0, mod( color_index, 8.0 ) / 7.0, 0.5 );
+		gl_Position= view_matrix * vec4( pos, 0.0, 1.0 );
+	}
+)";
+
+const char point_fragment[]=
+GLSL_VERSION
+R"(
+	in vec4 f_color;
+	out vec4 color;
+	void main()
+	{
+		vec2 r= gl_PointCoord * 2.0 - vec2( 1.0, 1.0 );
+		if( dot( r, r ) > 1.0 )
+			discard;
+		color= f_color * step( 0.0, r.x * r.y );
+	}
+)";
+
+const char linear_vertex[]=
+GLSL_VERSION
+R"(
+	uniform sampler1D tex;
+	uniform mat4 view_matrix;
+	in vec2 pos;
+	in float color_index;
+	out vec4 f_color;
+	void main()
+	{
+		f_color= texelFetch( tex, int(color_index), 0 );
+		gl_Position= view_matrix * vec4( pos, 0.0, 1.0 );
+	}
+)";
+
+const char linear_fragment[]=
+GLSL_VERSION
+R"(
+	in vec4 f_color;
+	out vec4 color;
+	void main()
+	{
+		color= f_color;
+	}
+)";
+
+const char areal_vertex[]=
+GLSL_VERSION
+R"(
+	uniform sampler1D tex;
+	uniform mat4 view_matrix;
+	in vec2 pos;
+	in float color_index;
+	out vec4 f_color;
+	void main()
+	{
+		f_color= texelFetch( tex, int(color_index), 0 );
+		gl_Position= view_matrix * vec4( pos, 0.0, 1.0 );
+	}
+)";
+
+const char areal_fragment[]=
+GLSL_VERSION
+R"(
+	in vec4 f_color;
+	out vec4 color;
+	void main()
+	{
+		color= f_color;
+	}
+)";
+
+} // namespace Shaders
+
+} // namespace PanzerMaps
---
+++ source/maps/shaders.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace PanzerMaps
+{
+
+namespace Shaders
+{
+
+extern const char point_vertex[];
+extern const char point_fragment[];
+
+extern const char linear_vertex[];
+extern const char linear_fragment[];
+
+extern const char areal_vertex[];
+extern const char areal_fragment[];
+
+} // namespace Shaders
+
+} // namespace PanzerMaps
