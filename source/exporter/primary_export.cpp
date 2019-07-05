003a87296eb191af878e9c4a47a6f4f252c73b9a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-07-05 14:02:32 +0700

    Ne vygružajem kanaly kak ploscadniki.

--- source/exporter/primary_export.cpp
+++ source/exporter/primary_export.cpp
@@ -221,8 +221,7 @@ WayClassifyResult ClassifyWay( const tinyxml2::XMLElement& way_element, const bo
 	{
 		if( std::strcmp( waterway, "stream" ) == 0 )
 			result.linear_object_class= LinearObjectClass::Waterway;
-		if( std::strcmp( waterway, "riverbank" ) == 0 ||
-			std::strcmp( waterway, "canal" ) == 0 )
+		if( std::strcmp( waterway, "riverbank" ) == 0 )
 			result.areal_object_class= ArealObjectClass::Water;
 	}
 	else if( const char* const railway= GetTagValue( way_element, "railway" ) )
