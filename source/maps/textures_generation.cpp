58e3ca65e0985fd1548193e986bc11f7c12c139e
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-07-09 18:08:48 +0700

    Realizovana generaçiaj tekstury knopki "GPS".

--- source/maps/textures_generation.cpp
+++ source/maps/textures_generation.cpp
@@ -1,4 +1,4 @@
-#include <cstring>
+﻿#include <cstring>
 #include <vector>
 #include "textures_generation.hpp"
 
@@ -11,6 +11,7 @@ namespace TexConstants
 static const unsigned char circle_color[4]{ 255, 255, 255, 200 };
 static const unsigned char circle_color_pressed[4]{ 200, 200, 200, 200 };
 static const unsigned char plus_color[4]{ 128, 128, 128, 200 };
+static const unsigned char active_gps_symbol_color[4]{ 128, 128, 255, 200 };
 
 } // TexConstants
 
@@ -98,26 +99,62 @@ r_Texture GenTexture_ZoomMinusPressed( const size_t tex_size )
 	return GenTexture_ZoomMinus_impl( tex_size, TexConstants::circle_color_pressed );
 }
 
-r_Texture GenTexture_GPSButtonActive( const size_t tex_size )
+static r_Texture GenTexture_GPSButton_impl( const size_t tex_size, const unsigned char* const symbol_color )
 {
 	std::vector<unsigned char> tex_data( tex_size * tex_size * 4u );
 
 	GenCircle( tex_size, TexConstants::circle_color, tex_data.data() );
 
-	// TODO - generate GPS symbol.
+	// Square.
+	for( size_t y= tex_size * 4u / 16u; y < tex_size * 5u / 16u; ++y )
+	for( size_t x= tex_size / 4u; x < tex_size * 3u / 4u; ++x )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	for( size_t y= tex_size * 11u / 16u; y < tex_size * 12u / 16u; ++y )
+	for( size_t x= tex_size / 4u; x < tex_size * 3u / 4u; ++x )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	for( size_t y= tex_size / 4u; y < tex_size * 3u / 4u; ++y )
+	for( size_t x= tex_size * 4u / 16u; x < tex_size * 5u / 16u; ++x )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	for( size_t y= tex_size / 4u; y < tex_size * 3u / 4u; ++y )
+	for( size_t x= tex_size * 11u / 16u; x < tex_size * 12u / 16u; ++x )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	// Cross bars
+	for( size_t y= tex_size * 15u / 32u; y < tex_size * 17u / 32u; ++y )
+	for( size_t x= tex_size * 2u / 16u; x < tex_size * 4u / 16u; ++x )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	for( size_t y= tex_size * 15u / 32u; y < tex_size * 17u / 32u; ++y )
+	for( size_t x= tex_size * 12u / 16u; x < tex_size * 14u / 16u; ++x )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	for( size_t x= tex_size * 15u / 32u; x < tex_size * 17u / 32u; ++x )
+	for( size_t y= tex_size * 2u / 16u; y < tex_size * 4u / 16u; ++y )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	for( size_t x= tex_size * 15u / 32u; x < tex_size * 17u / 32u; ++x )
+	for( size_t y= tex_size * 12u / 16u; y < tex_size * 14u / 16u; ++y )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
+
+	// Dot
+	for( size_t x= tex_size * 7u / 16u; x < tex_size * 9u / 16u; ++x )
+	for( size_t y= tex_size * 7u / 16u; y < tex_size * 9u / 16u; ++y )
+		std::memcpy( tex_data.data() + 4u * ( x + y * tex_size ), symbol_color, sizeof(unsigned char) * 4u );
 
 	return CreateTexture( tex_size, tex_size, tex_data );
 }
 
+r_Texture GenTexture_GPSButtonActive( const size_t tex_size )
+{
+	return GenTexture_GPSButton_impl( tex_size, TexConstants::active_gps_symbol_color );
+}
+
 r_Texture GenTexture_GPSButtonUnactive( const size_t tex_size )
 {
-	std::vector<unsigned char> tex_data( tex_size * tex_size * 4u );
-
-	GenCircle( tex_size, TexConstants::circle_color_pressed, tex_data.data() );
-
-	// TODO - generate GPS symbol.
-
-	return CreateTexture( tex_size, tex_size, tex_data );
+	return GenTexture_GPSButton_impl( tex_size, TexConstants::plus_color );
 }
 
 } // namespace PanzerMaps
