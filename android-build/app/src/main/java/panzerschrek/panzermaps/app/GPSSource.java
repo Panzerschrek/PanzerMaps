392de097e317c1f7d8d9fb24d60e7fed1c769be6
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-07-10 09:25:13 +0700

    Delajem kod polucenija geopoziçii potokobezopasnym.

--- android-build/app/src/main/java/panzerschrek/panzermaps/app/GPSSource.java
+++ android-build/app/src/main/java/panzerschrek/panzermaps/app/GPSSource.java
@@ -7,8 +7,13 @@ import android.util.Log;
 
 public class GPSSource
 {
-	public static void Enable( android.app.Activity activity )
+	public static void Enable( Activity activity )
 	{
+		if( activity == null )
+		{
+			Log.w( TAG, "null activity" );
+			return;
+		}
 		activity_= activity;
 		activity.runOnUiThread(
 			new Runnable()
@@ -17,20 +22,27 @@ public class GPSSource
 				{
 					try // Catch all exceptions on UiThread. We do not want to ruin it.
 					{
-						location_manager_= (android.location.LocationManager) activity_.getSystemService( activity_.LOCATION_SERVICE );
-						if( location_manager_ == null )
+						Activity activity= activity_;
+						if( activity == null )
+						{
+							return;
+						}
+
+						LocationManager location_manager= (LocationManager) activity.getSystemService( activity.LOCATION_SERVICE );
+						if( location_manager == null )
 						{
 							Log.w( TAG, "can not get location manager" );
 							return;
 						}
+						location_manager_= location_manager;
 						Log.i( TAG, "get location manager" );
 
 						String provider= LocationManager.GPS_PROVIDER;
 
-						last_location_= location_manager_.getLastKnownLocation( provider );
+						last_location_= location_manager.getLastKnownLocation( provider );
 						Log.i( TAG, "get last known location" );
 
-						location_listener_= new LocationListener()
+						LocationListener location_listener= new LocationListener()
 						{
 							@Override
 							public void onLocationChanged(android.location.Location location) { last_location_= location; }
@@ -45,7 +57,8 @@ public class GPSSource
 							public void onProviderDisabled(String provider) {}
 						};
 
-						location_manager_.requestLocationUpdates( provider, 2000, 5.0f, location_listener_ );
+						location_manager.requestLocationUpdates( provider, 2000, 5.0f, location_listener );
+						location_listener_= location_listener;
 						Log.i( TAG, "subscribe to updates" );
 					}
 					catch( Exception e )
@@ -58,20 +71,28 @@ public class GPSSource
 
 	public static void Disable()
 	{
-		activity_.runOnUiThread(
+		Activity activity= activity_;
+		if( activity == null )
+		{
+			return;
+		}
+		activity.runOnUiThread(
 			new Runnable()
 			{
 				public void run()
 				{
 					try // Catch all exceptions on UiThread. We do not want to ruin it.
 					{
-						if( location_manager_ != null && location_listener_ != null )
+						LocationManager location_manager= location_manager_;
+						LocationListener location_listener= location_listener_;
+						if( location_manager != null && location_listener != null )
 						{
-							location_manager_.removeUpdates(location_listener_);
+							location_manager.removeUpdates(location_listener);
 						}
-						location_listener_= null;
-						location_listener_= null;
 						activity_= null;
+						location_manager_= null;
+						location_listener_= null;
+						last_location_= null;
 					}
 					catch( Exception e )
 					{
@@ -83,22 +104,26 @@ public class GPSSource
 
 	public static double GetLongitude()
 	{
-		if( last_location_ == null )
-			return 1000.0;
-		return last_location_.getLongitude();
+		Location location= last_location_;
+		if( location == null )
+			return c_invalid_coordinate;
+		return location.getLongitude();
 	}
 
 	public static double GetLatitude()
 	{
-		if( last_location_ == null )
-			return 1000.0;
-		return last_location_.getLatitude();
+		Location location= last_location_;
+		if( location == null )
+			return c_invalid_coordinate;
+		return location.getLatitude();
 	}
 
 	private static final String TAG = "PanzerMaps";
+	public static final double c_invalid_coordinate= 1000.0;
 
-	static private Activity activity_;
-	static private android.location.LocationManager location_manager_;
-	static private android.location.Location last_location_;
-	static private LocationListener location_listener_;
+	// All variables are volatile, because we can access them from different threads.
+	static private volatile Activity activity_;
+	static private volatile LocationManager location_manager_;
+	static private volatile LocationListener location_listener_;
+	static private volatile Location last_location_;
 }
